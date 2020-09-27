2e33934d7863ce1203bd1d11ee289136ce2f4ad3
Kleidson <kleidson.ar@gmail.com>
2020-09-26 21:49:46 -0300

    Create Pagina1.jsx

---
+++ exemplos/Pagina1.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import { Button } from 'react-native-paper';
+
+export default ({navigation}) => {
+    return (
+       <>
+            <View>
+                <Text>Página 1</Text>
+                <Text>1</Text>
+
+                <Button onPress={()=>navigation.navigate('p2')} >Ir Para a Página 2</Button>
+            </View>
+       </> 
+    )
+}
